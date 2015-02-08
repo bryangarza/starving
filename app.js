7611273a638069b078485f2c02ddffb49b8bb755
Bryan Garza <brygarza@gmail.com>
2015-02-07 23:04:57 -0800

    Routes for both following and stars, JSON format

--- app.js
+++ app.js
@@ -14,9 +14,21 @@ var pub = path.join(__dirname, '/public')
 app.use(express.static(pub))
 app.use(morgan('dev'))
 
-app.get('/user/:id/following', function(req, res) {
-  /* gh.user.getFollowingFromUser({ */
-  gh.misc.rateLimit({}, function(err, ghres) {
+app.get('/json/user/:id/following', function(req, res) {
+  gh.user.getFollowingFromUser({
+    user: req.params.id
+  }, function(err, ghres) {
+    if (err) {
+      console.log(err.stack)
+    }
+    res.json(JSON.stringify(ghres))
+  })
+})
+
+app.get('/json/user/:id/stars', function(req, res) {
+  gh.repos.getStarredFromUser({
+    user: req.params.id
+  }, function(err, ghres) {
     if (err) {
       console.log(err.stack)
     }
