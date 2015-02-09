3e8ad4521f9b3c83f27665cee5efa1b7b829a3f4
Bryan Garza <brygarza@gmail.com>
2015-02-09 10:48:18 -0800

    Switch to using semicolons

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-'use strict'
-var path = require('path')
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 8080
-var GithubApi = require('github')
+'use strict';
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var GithubApi = require('github');
 var gh = new GithubApi({
   version: '3.0.0'
-})
+});
 
-var morgan = require('morgan')
-var pub = path.join(__dirname, '/public')
+var morgan = require('morgan');
+var pub = path.join(__dirname, '/public');
 
-app.use(express.static(pub))
-app.use(morgan('dev'))
+app.use(express.static(pub));
+app.use(morgan('dev'));
 
 app.get('/json/user/:id/following', function(req, res) {
   gh.user.getFollowingFromUser({
     user: req.params.id
   }, function(err, ghres) {
     if (err) {
-      console.log(err.stack)
+      console.log(err.stack);
     }
-    res.json(JSON.stringify(ghres))
-  })
-})
+    res.json(JSON.stringify(ghres));
+  });
+});
 
 app.get('/json/user/:id/stars', function(req, res) {
   gh.repos.getStarredFromUser({
     user: req.params.id
   }, function(err, ghres) {
     if (err) {
-      console.log(err.stack)
+      console.log(err.stack);
     }
-    res.json(JSON.stringify(ghres))
-  })
-})
+    res.json(JSON.stringify(ghres));
+  });
+});
 
 var server = app.listen(port, function() {
   console.log('Listening at http://%s:%s',
               server.address().address,
-              server.address().port)
-})
+              server.address().port);
+});
