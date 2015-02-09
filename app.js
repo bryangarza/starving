789d0f4410fb8417ea71c632f09250f60a93a3fc
Bryan Garza <brygarza@gmail.com>
2015-02-09 13:06:27 -0800

    Go back to no semicolons now that I replaced web-mode's js with normal js-mode, the `o` operator in evil-mode now indents correctly.

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-'use strict';
-var path = require('path');
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var GithubApi = require('github');
+'use strict'
+var path = require('path')
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
+var GithubApi = require('github')
 var gh = new GithubApi({
   version: '3.0.0'
-});
+})
 
-var morgan = require('morgan');
-var pub = path.join(__dirname, '/public');
+var morgan = require('morgan')
+var pub = path.join(__dirname, '/public')
 
-app.use(express.static(pub));
-app.use(morgan('dev'));
+app.use(express.static(pub))
+app.use(morgan('dev'))
 
-app.set('view engine', 'jade');
+app.set('view engine', 'jade')
 if (app.get('env') === 'development') {
-  app.locals.pretty = true;
+  app.locals.pretty = true
 }
 
 app.get('/:user', function(req, res) {
   gh.repos.getStarredFromUser({ user: req.params.user}, function(err, ghres) {
     if (err) {
-      console.log(err.stack);
+      console.log(err.stack)
     }
-    res.render('user', { user: ghres });
-  });
-});
+    res.render('user', { user: ghres })
+  })
+})
 
 app.get('/:user/f', function(req, res) {
   gh.user.getFollowingFromUser({ user: req.params.user}, function(err, ghres) {
     if (err) {
-      console.log(err.stack);
+      console.log(err.stack)
     }
-    res.render('following', { user: ghres });
-  });
-});
+    res.render('following', { user: ghres })
+  })
+})
 
 var server = app.listen(port, function() {
   console.log('Listening at http://%s:%s',
               server.address().address,
-              server.address().port);
-});
+              server.address().port)
+})
