a2b6e3266fefb1268ad208e3c7380424a970497a
Bryan Garza <brygarza@gmail.com>
2015-02-09 11:43:41 -0800

    Set up jade

--- app.js
+++ app.js
@@ -14,12 +14,17 @@ var pub = path.join(__dirname, '/public');
 app.use(express.static(pub));
 app.use(morgan('dev'));
 
+app.set('view engine', 'jade');
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+
 app.get('/:user', function(req, res) {
   gh.repos.getStarredFromUser({ user: req.params.user}, function(err, ghres) {
     if (err) {
       console.log(err.stack);
     }
-    res.json(JSON.stringify(ghres));
+    res.render('user', { user: ghres });
   });
 });
 
@@ -28,7 +33,7 @@ app.get('/:user/f', function(req, res) {
     if (err) {
       console.log(err.stack);
     }
-    res.json(JSON.stringify(ghres));
+    res.render('following', { user: ghres });
   });
 });
 
