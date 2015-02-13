60b6a917973356aa34df7515ce2be1e1c26e03d1
Bryan Garza <brygarza@gmail.com>
2015-02-12 22:52:50 -0800

    Get display to look nicer, set up real routes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var path = require('path')
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
-var gh = require('./mock/githubmock.js')
-// var GithubApi = require('github')
-// var gh = new GithubApi({
-//   version: '3.0.0'
-// })
+// var gh = require('./mock/githubmock.js')
+var GithubApi = require('github')
+var gh = new GithubApi({
+  version: '3.0.0'
+})
 
 var morgan = require('morgan')
 var pub = path.join(__dirname, '/public')
@@ -20,41 +20,25 @@ if (app.get('env') === 'development') {
   app.locals.pretty = true
 }
 
-// Reminder: change this route back to /:user
-app.get('/', function(req, res) {
-  // gh.repos.getStarredFromUser({ user: req.params.user}, function(err, ghres) {
-  gh.getStarredFromUser({ user: req.params.user}, function(err, ghres) {
+app.get('/:user', function(req, res) {
+  gh.repos.getStarredFromUser({ user: req.params.user}, function(err, ghres) {
     if (err) console.log(err.stack)
     res.render('starred', { stars: ghres })
   })
 })
 
 
-// This is how I saved the API responses to mock them later
-
 // app.get('/:user/following', function(req, res) {
 //   gh.user.getFollowingFromUser({ user: req.params.user}, function(err, ghres) {
-//     if (err) {
-//       console.log(err.stack)
-//     }
-//     fs.writeFile('./getFollowingFromUser', JSON.stringify(ghres), function(err) {
-//       if (err) throw err
-//       console.log('wrote res to file, following')
-//     })
-//     // res.render('following', { user: ghres })
+//     if (err) console.log(err.stack)
+//     res.render('following', { users: ghres })
 //   })
 // })
 
 // app.get('/:user/followers', function(req, res) {
 //   gh.user.getFollowers({ user: req.params.user}, function(err, ghres) {
-//     if (err) {
-//       console.log(err.stack)
-//     }
-//     fs.writeFile('./getFollowers', JSON.stringify(ghres), function(err) {
-//       if (err) throw err
-//       console.log('wrote res to file, followers')
-//     })
-//     // res.render('followers', { user: ghres })
+//     if (err) console.log(err.stack)
+//     res.render('followers', { users: ghres })
 //   })
 // })
 
